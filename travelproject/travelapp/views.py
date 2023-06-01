e9249b76fba53d1d6bfe9bc718ab41d1d28d96d8
nandakishor <nandakishorernair2002@gmail.com>
2023-06-01 09:52:18 +0530

    added

---
+++ travelproject/travelapp/views.py
@@ -0,0 +1,15 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+from . models import Place, Destinations
+
+# Create your views here.
+def demo(request):
+    obj=Place.objects.all()
+    fun = Destinations.objects.all()
+    return render(request,"index.html",{'result':obj,'results':fun})
+
+# def demo(request):
+#     fun=Destinations.objects.all()
+#     return render(request,"index.html",{'results':fun})
+
+
